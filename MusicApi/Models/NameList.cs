b3a831a8d02ad1e37b2df74733c8f2f37be45bcc
Yegen Govender <yegengovender@gmail.com>
2014-06-13 13:17:00 +0200

    hopefully fixed the conflicts

--- MusicApi/Models/NameList.cs
+++ MusicApi/Models/NameList.cs
@@ -1,7 +1,11 @@
-﻿namespace MusicApi.Models
+﻿using System.Runtime.Serialization;
+
+namespace MusicApi.Models
 {
+    [DataContract]
     public class NameList
     {
+        [DataMember]
         public string Name { get; set; }
 
         public NameList(string name)
